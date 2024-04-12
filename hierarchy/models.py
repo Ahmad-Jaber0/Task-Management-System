a709d26773ebf6f2774f8f5efdd1826a85803339
Ahmad-Jaber0 <jaber5834@gmail.com>
2024-04-12 16:58:13 +0200

    Create DB

--- hierarchy/models.py
+++ hierarchy/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    role = models.CharField(max_length=20)
+
+
+class Task(models.Model):
+    task_description = models.TextField()
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20)    
--- project/settings.py
+++ project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'hierarchy'
+    'hierarchy',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
